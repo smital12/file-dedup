ebba4662abf9bd2d14d111a621a1cc699b9e5ad0
Alex Smith Torres <>
2017-08-04 16:33:15 -0700

    Implemented function to return lists of duplicate java Files

--- src/file_dedup/core.clj
+++ src/file_dedup/core.clj
@@ -1,18 +1,26 @@
 (ns file-dedup.core
   (:require [clojure.java.io :as io]))
 
+(def file-string "./src/file_dedup/")
+;(def file-string "C:/Strawberry/c/bin" )
 (def usage "Usage: file-dedup dir1 dir2")
 
-(defn get-files-in-dir [d]
-  "lists the files in a directory"
+
+(defn find-duplicate-files [d]
+  "recursively lists all files in a directory"
   (->> (file-seq (io/file d))
-       (filter #(.isFile %))))
+       (filter #(.isFile %))
+       (map (fn [file] {(str (.getName file)) [file]}))
+       (apply merge-with concat)
+       vals
+       (filter #(> (count %) 1))))
 
 
 (defn file-dedup
   "list files in a directory"
   ([] usage)
-  ([dir]
-   (clojure.pprint/pprint ( get-files-in-dir dir ))))
+  ([dir] (find-duplicate-files dir)))
 
-(file-dedup "C:/Users/c17722/Documents/workspace/clojure/file-dedup/src/file_dedup/")
+
+;(file-dedup file-string)
+(clojure.pprint/pprint (find-duplicate-files file-string))
